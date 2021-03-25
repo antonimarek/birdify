06856d7efd4a80a6a2859af87a7eb1148a931e09
Antoni Marek <antoni.m.marek@gmail.com>
2021-03-25 20:20:18 +0100

    signal plotting

--- audio_processing.py
+++ audio_processing.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 
 class Visualize:
-    def __init__(self, signal: dict):
+    def __init__(self, signal: dict, fft: dict):
         self.signal = signal
+        self.fft = fft
 
-    def signal_plot(self, title: str = 'Time series'):
-        key_list = list(self.signal.keys())
+    def plot_prepare(self, key_list: list, data: dict, title: str = 'Time series'):
         item_num = len(key_list)
         n_col = 2
         n_row = int(np.ceil(item_num / n_col))
@@ -19,11 +19,16 @@ class Visualize:
             for y in range(n_col):
                 if i < item_num:
                     ax = plt.subplot2grid((n_row, n_col), (x, y))
-                    ax.plot(self.signal[key_list[i]])
+                    ax.plot(data[key_list[i]])
                     ax.set_title(key_list[i])
                     ax.get_xaxis().set_visible(False)
                     ax.get_yaxis().set_visible(False)
                     i += 1
+        return self
+
+    def signal_plot(self):
+        key_list = list(self.signal.keys())
+        self.plot_prepare(key_list, self.signal)
         plt.show()
         pass
 
