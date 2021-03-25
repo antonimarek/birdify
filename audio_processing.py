87fbf9f1f11e6bcf24479d1e8b49a23333d99dbe
Antoni Marek <antoni.m.marek@gmail.com>
2021-03-25 20:55:06 +0100

    plotting upgrade

--- audio_processing.py
+++ audio_processing.py
@@ -3,34 +3,43 @@ import matplotlib.pyplot as plt
 
 
 class Visualize:
-    def __init__(self, signal: dict, fft: dict):
+    def __init__(self, signal: dict = None, fft: dict = None):
         self.signal = signal
         self.fft = fft
 
-    def plot_prepare(self, key_list: list, data: dict, title: str = 'Time series'):
+    def plot_prepare(self, plot_size: tuple, key_list: list, x_arg: list, y_arg: list = False,
+                     title: str = 'Time series', n_col: int = 2):
         item_num = len(key_list)
-        n_col = 2
         n_row = int(np.ceil(item_num / n_col))
 
-        fig, _ = plt.subplots(figsize=(20, 5))
+        fig, _ = plt.subplots(figsize=plot_size, sharey='all')
         fig.suptitle(title, size=16)
         i = 0
         for x in range(n_row):
             for y in range(n_col):
                 if i < item_num:
                     ax = plt.subplot2grid((n_row, n_col), (x, y))
-                    ax.plot(data[key_list[i]])
+                    if y_arg:
+                        ax.plot(x_arg[i], y_arg[i])
+                    else:
+                        ax.plot(x_arg[i])
                     ax.set_title(key_list[i])
                     ax.get_xaxis().set_visible(False)
                     ax.get_yaxis().set_visible(False)
                     i += 1
         return self
 
-    def signal_plot(self):
+    def signal_plot(self, plot_size: tuple = (20, 5), n_col: int = 2):
         key_list = list(self.signal.keys())
-        self.plot_prepare(key_list, self.signal)
+        x_arg = [self.signal[key] for key in key_list]
+        self.plot_prepare(plot_size, key_list, x_arg, n_col=n_col)
         plt.show()
         pass
 
-    def fft_plt(self):
-        return self
+    def fft_plt(self, plot_size: tuple = (20, 5), n_col: int = 2):
+        key_list = list(self.fft.keys())
+        x_arg = [self.fft[key][1] for key in key_list]
+        y_arg = [self.fft[key][0] for key in key_list]
+        self.plot_prepare(plot_size, key_list, x_arg, y_arg, n_col=n_col)
+        plt.show()
+        pass
