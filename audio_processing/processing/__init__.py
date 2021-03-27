324491805b4fd56c5fe578897a08ea52f5afde1b
Antoni Marek <antoni.m.marek@gmail.com>
2021-03-28 00:15:40 +0100

    sound info func implementation

--- audio_processing/processing/__init__.py
+++ audio_processing/processing/__init__.py
@@ -1,1 +1,1 @@
-from . import *
+from .data_operation import *
--- audio_processing/processing/data_operation.py
+++ audio_processing/processing/data_operation.py
@@ -1,7 +1,19 @@
 from pydub import AudioSegment
 import librosa
 import sox
+import pandas as pd
+import numpy as np
 
-# input column with paths, in return columns with all added info (True -> return particular, False -> don't)
 
-# sample_rate = sox.file_info.sample_rate('path/to/file.mp3')
+def add_sample_info(df, path_column: str, s_rate: bool = True, n_samples: bool = True, dur: bool = True):
+    sample_info = pd.DataFrame()
+
+    for f in df.index:
+        if s_rate:
+            sample_info.loc[f, 'sample_rate'] = sox.file_info.sample_rate(df.loc[f, path_column])
+        if n_samples:
+            sample_info.loc[f, 'num_samples'] = sox.file_info.num_samples(df.loc[f, path_column])
+        if dur:
+            sample_info.loc[f, 'duration'] = sox.file_info.duration(df.loc[f, path_column])
+    sample_info[['sample_rate', 'num_samples']] = sample_info[['sample_rate', 'num_samples']].applymap(int)
+    return sample_info
