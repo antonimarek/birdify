469b9541a98539c11591e25c68d04f8318035feb
Antoni Marek <antoni.m.marek@gmail.com>
2021-03-27 22:09:01 +0100

    sox resolved, faster sound info

--- audio_processing/processing/data_operation.py
+++ audio_processing/processing/data_operation.py
@@ -2,10 +2,6 @@ from pydub import AudioSegment
 import librosa
 import sox
 
-# def fs_from_mp3(path):
-#     signal = AudioSegment.from_mp3(path)
-#     fs = signal.frame_rate
-#     signal, _ = librosa.load(path, sr=fs)
-#     data.at[f, 'Time'] = signal.shape[0]/fs
+# input column with paths, in return columns with all added info (True -> return particular, False -> don't)
 
-sample_rate = sox.file_info.sample_rate('path/to/file.mp3')
+# sample_rate = sox.file_info.sample_rate('path/to/file.mp3')
