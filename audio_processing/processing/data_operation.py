9db5689d21e20a81aa2fb606a31a89ea039f5ec5
Antoni Marek <antoni.m.marek@gmail.com>
2021-03-28 22:33:07 +0200

    changes, changes

--- audio_processing/processing/data_operation.py
+++ audio_processing/processing/data_operation.py
@@ -3,6 +3,7 @@ import librosa
 import sox
 import pandas as pd
 import numpy as np
+from python_speech_features import mfcc, logfbank
 
 
 def add_sample_info(df, path_column: str, s_rate: bool = True, n_samples: bool = True, dur: bool = True):
@@ -15,5 +16,21 @@ def add_sample_info(df, path_column: str, s_rate: bool = True, n_samples: bool =
             sample_info.loc[f, 'num_samples'] = sox.file_info.num_samples(df.loc[f, path_column])
         if dur:
             sample_info.loc[f, 'duration'] = sox.file_info.duration(df.loc[f, path_column])
-    sample_info[['sample_rate', 'num_samples']] = sample_info[['sample_rate', 'num_samples']].applymap(int)
+    if s_rate:
+        sample_info.sample_rate = sample_info.sample_rate.map(int)
+    if n_samples:
+        sample_info.num_samples = sample_info.num_samples.map(int)
     return sample_info
+
+
+def fft_calc(y, rate: int):
+    """
+    Calculate fft.
+    :param y: signal as numpy array
+    :param rate: sampling rate as int
+    :return: (magnitude, frequency values)
+    """
+    n = len(y)
+    freq = np.fft.rfftfreq(n, d=1 / rate)
+    magnitude = abs(np.fft.rfft(y) / n)
+    return magnitude, freq
--- exploring_dataset.py
+++ exploring_dataset.py
@@ -8,6 +8,7 @@ Created on Sat Mar 27 20:26:21 2021
 from python_speech_features import mfcc, logfbank
 import librosa
 import audio_processing as ap
+from audio_processing.processing import add_sample_info, fft_calc
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -15,26 +16,61 @@ from pytimeparse.timeparse import timeparse
 from pydub import AudioSegment
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pickle
+from datetime import datetime, timedelta
 
 data = pd.read_csv(r'metadata.csv')
 data['Name'] = data.Genus +' '+ data.Specific_epithet
 
-data.set_index('Path', inplace=True)
-data.Time = data.Time.where(data.Time.str.contains(':'))
-data.Time = data.Time.apply(lambda x: timeparse(str(x)))
+def timedeltas(x):
+    t = datetime.strptime(x, '%M:%S').time()
+    return timedelta(minutes=t.minute, seconds=t.second)
 
-for f in data[data.Time.isna()].index:
-    signal = AudioSegment.from_mp3(f)
-    fs = signal.frame_rate
-    signal, _ = librosa.load(f, sr=fs)
-    data.at[f, 'Time'] = signal.shape[0]/fs
+data['duration'] = data['Length'].apply(lambda x: timedeltas(x).total_seconds())
 
-plotting = data.groupby(['Name'])['Time'].mean()
+plotting = data.groupby(['Name'])['duration'].mean()
 
-plt.figure(figsize=(15,5))
-sns.barplot(x=plotting.index, y=plotting.values)
-plt.xticks(rotation = 90)
-plt.xlabel(None)
-plt.ylabel('mean time [s]')
-plt.title('Class distribution', fontdict={'size':16}, y=1.05)
-plt.show()
+# plt.figure(figsize=(15,5))
+# sns.barplot(x=plotting.index, y=plotting.values, saturation=0.6)
+# plt.xticks(rotation = 90)
+# plt.xlabel(None)
+# plt.ylabel('mean time [s]')
+# plt.title('Class distribution', fontdict={'size':16}, y=1.05)
+# plt.show()
+
+# data[['sample_rate', 'num_samples']] = add_sample_info(data, 'Path', dur=False)
+
+# with open('dataset.pkl', 'wb') as f:
+#     pickle.dump(data, f)
+
+with open('dataset.pkl', 'rb') as f:
+    data = pickle.load(f)
+
+classes = data.Name.unique()
+
+signals = {}
+fft = {}
+fbank = {}
+mfccs = {}
+
+for c in classes[:1]:
+    ix = data[data.Name == c].index[-1]
+    f = data.loc[ix].Path
+    fs = data.loc[ix].sample_rate
+    signal, rate = librosa.load(f, sr=fs)
+    signals[c] = signal
+    fft[c] = fft_calc(signal, rate)
+
+# for c in class_names:
+#     mp3_file = data[data.Name == c].iloc[0, 0]
+#     song = AudioSegment.from_mp3(file)
+#     fs = song.frame_rate
+#     signal, _ = librosa.load(file, sr=fs)
+#     signals[c] = signal
+#     fft[c] = calc_fft(signal, fs)
+
+#     bank = logfbank(signal[:fs], fs, nfilt=128, nfft=1200).T
+#     fbank[c] = bank
+#     mel = mfcc(signal[:fs], fs, numcep=64, nfilt=128, nfft=1200).T
+#     mfccs[c] = mel
+               
