217715701583d952467e2f347c7c4559300a1784
Antoni Marek <antoni.m.marek@gmail.com>
2021-03-31 22:03:56 +0200

    using pydub

--- audio_processing/processing/data_operation.py
+++ audio_processing/processing/data_operation.py
@@ -1,36 +1,58 @@
-from pydub import AudioSegment
-import librosa
-import sox
+# import librosa
+# import sox
 import pandas as pd
 import numpy as np
-from python_speech_features import mfcc, logfbank
+from pydub import AudioSegment
+import os
+# from python_speech_features import mfcc, logfbank
 
 
-def add_sample_info(df, path_column: str, s_rate: bool = True, n_samples: bool = True, dur: bool = True):
-    sample_info = pd.DataFrame()
+# def add_sample_info(df, path_column: str, s_rate: bool = True, n_samples: bool = True, dur: bool = True):
+#     sample_info = pd.DataFrame()
+#
+#     for f in df.index:
+#         if s_rate:
+#             sample_info.loc[f, 'sample_rate'] = sox.file_info.sample_rate(df.loc[f, path_column])
+#         if n_samples:
+#             sample_info.loc[f, 'num_samples'] = sox.file_info.num_samples(df.loc[f, path_column])
+#         if dur:
+#             sample_info.loc[f, 'duration'] = sox.file_info.duration(df.loc[f, path_column])
+#     if s_rate:
+#         sample_info.sample_rate = sample_info.sample_rate.map(int)
+#     if n_samples:
+#         sample_info.num_samples = sample_info.num_samples.map(int)
+#     return sample_info
 
-    for f in df.index:
-        if s_rate:
-            sample_info.loc[f, 'sample_rate'] = sox.file_info.sample_rate(df.loc[f, path_column])
-        if n_samples:
-            sample_info.loc[f, 'num_samples'] = sox.file_info.num_samples(df.loc[f, path_column])
-        if dur:
-            sample_info.loc[f, 'duration'] = sox.file_info.duration(df.loc[f, path_column])
-    if s_rate:
-        sample_info.sample_rate = sample_info.sample_rate.map(int)
-    if n_samples:
-        sample_info.num_samples = sample_info.num_samples.map(int)
-    return sample_info
+
+def resample_to_file(in_path: str, out_path: str, in_format: str = 'mp3', out_format: str = 'mp3', sr: int = 22050):
+    sample = AudioSegment.from_file(in_path, in_format)
+    sample = sample.set_frame_rate(sr)
+    sample.export(out_path, out_format)
+    pass
 
 
-def fft_calc(y, rate: int):
-    """
-    Calculate fft.
-    :param y: np.ndarray
-    :param rate: sampling rate as int
-    :return: (magnitude, frequency values)
-    """
-    n = len(y)
-    freq = np.fft.rfftfreq(n, d=1 / rate)
-    magnitude = abs(np.fft.rfft(y) / n)
-    return magnitude, freq
+def resample_dir(dir_path_in: str, dir_path_out: str, in_format: str = 'mp3', out_format: str = 'mp3'):
+    f_list = os.listdir(dir_path_in)
+    for file in f_list:
+        in_path = dir_path_in + '/' + file
+        out_path = dir_path_out + '/' + file
+        resample_to_file(in_path=in_path, out_path=out_path, in_format=in_format, out_format=out_format)
+    return f'{len(f_list)} files from {dir_path_in} resampled into {dir_path_out}'
+
+
+# song = AudioSegment.from_mp3(r'./Sonus-naturalis-156938.mp3')
+# samples = song.get_array_of_samples()
+# samples = np.array(samples, dtype=float)
+
+
+# def fft_calc(y, rate: int):
+#     """
+#     Calculate fft.
+#     :param y: np.ndarray
+#     :param rate: sampling rate as int
+#     :return: (magnitude, frequency values)
+#     """
+#     n = len(y)
+#     freq = np.fft.rfftfreq(n, d=1 / rate)
+#     magnitude = abs(np.fft.rfft(y) / n)
+#     return magnitude, freq
