3c6691d6df376ec4e2508d105b08c10fcdb019d3
Antoni Marek <antoni.m.marek@gmail.com>
2021-04-09 23:15:35 +0200

    rewritten, added pitch shift + support for signal array

--- soundmatrix/processing/machine_learning/augmentation.py
+++ soundmatrix/processing/machine_learning/augmentation.py
@@ -1,5 +1,6 @@
 import numpy as np
 from soundmatrix.processing.extraction.feature_preparation import min_max_scale
+import librosa
 
 
 def normalize(sample):
@@ -9,14 +10,19 @@ def normalize(sample):
 
 
 def time_shift(sample):
-    s_len = sample.shape[1]
+    s_len = sample.shape[-1]
     div_loc = np.random.randint(0, s_len)
-    sample = np.concatenate((sample[:, div_loc:], sample[:, :div_loc]), axis=1)
+    if len(sample.shape) == 1:
+        return np.concatenate((sample[div_loc:], sample[:div_loc]))
+    elif len(sample.shape) == 2:
+        return np.concatenate((sample[:, div_loc:], sample[:, :div_loc]), axis=1)
+    else:
+        return ValueError("Sample can't have more than 2 dimensions!")
+
+
+def pitch_shift(sample, sr: int = 22050):
+    sample = librosa.effects.pitch_shift(y=sample, sr=sr, n_steps=np.random.random()*1, bins_per_octave=24)
     return sample
-
-
-def pitch_shift():
-    pass
 
 
 def combine_audio(sample_1, sample_2):
@@ -33,5 +39,16 @@ def add_noise(sample, noise, d_fac=.4):
     return sample
 
 
-def augmentation(sample):
-    pass
+def augment_audio(sample, noise_sample=None, comb_sample=None, sr: int = 22050):
+    sample = time_shift(sample)
+    sample = pitch_shift(sample, sr=sr)
+    if comb_sample is not None:
+        comb_sample = time_shift(comb_sample)
+        comb_sample = pitch_shift(comb_sample, sr=sr)
+        sample = combine_audio(sample, comb_sample)
+    if noise_sample is not None:
+        noise_sample = time_shift(noise_sample)
+        noise_sample = pitch_shift(noise_sample, sr=sr)
+        sample = add_noise(sample, noise_sample)
+    sample = min_max_scale(sample)
+    return sample
