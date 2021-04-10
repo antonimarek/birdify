5e0e6643646edc6d1dba51af74c436a095a47e8f
Antoni Marek <antoni.m.marek@gmail.com>
2021-04-10 09:08:27 +0200

    updated multiplication method

--- soundmatrix/processing/machine_learning/augmentation.py
+++ soundmatrix/processing/machine_learning/augmentation.py
@@ -20,21 +20,26 @@ def time_shift(sample):
         return ValueError("Sample can't have more than 2 dimensions!")
 
 
-def pitch_shift(sample, sr: int = 22050):
-    sample = librosa.effects.pitch_shift(y=sample, sr=sr, n_steps=np.random.random()*1, bins_per_octave=24)
+def pitch_shift(sample, sr: int = 22050, half_tone_var: float = 2):
+    sample = librosa.effects.pitch_shift(y=sample, sr=sr,
+                                         n_steps=np.random.uniform(-half_tone_var, half_tone_var),
+                                         bins_per_octave=24)
     return sample
 
 
 def combine_audio(sample_1, sample_2):
-    w1 = np.random.random()
+    w1 = np.random.uniform(.5, 1)
     w2 = 1 - w1
     sample = normalize(sample_1) * w1 + normalize(sample_2) * w2
     sample = min_max_scale(sample)
     return sample
 
 
-def add_noise(sample, noise, d_fac=.4):
-    sample = normalize(sample) + normalize(noise) * d_fac * np.random.random()
+def add_noise(sample, noise, d_fac=.4, r_mltp: bool = True):
+    r_multiplier = 1
+    if r_mltp:
+        r_multiplier = np.random.random()
+    sample = normalize(sample) + normalize(noise) * d_fac * r_multiplier
     sample = min_max_scale(sample)
     return sample
 
@@ -49,6 +54,6 @@ def augment_audio(sample, noise_sample=None, comb_sample=None, sr: int = 22050):
     if noise_sample is not None:
         noise_sample = time_shift(noise_sample)
         noise_sample = pitch_shift(noise_sample, sr=sr)
-        sample = add_noise(sample, noise_sample)
+        sample = add_noise(sample, noise_sample, r_mltp=False)
     sample = min_max_scale(sample)
     return sample
